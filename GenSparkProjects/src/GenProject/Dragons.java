27dc149dfacf9920af41a1aa25c6a85aac806c66
Casey.T.Hanley <caseythomashanley@hotmail.com>
2023-01-14 18:07:53 -0600

    New Notes
    Made notes in Dragons

---
+++ GenSparkProjects/src/GenProject/Dragons.java
@@ -0,0 +1,39 @@
+package GenProject;
+
+public class Dragons {
+
+    public int DragonsCave(){
+
+        //Output Story:
+        System.out.println("You are in a long full of dragons. In front of you,");
+        System.out.println("");
+        System.out.println("you see two caves. In one, the dragon is friendly");
+        System.out.println("");
+        System.out.println("and will share his treasure with you/ The other dragon");
+        System.out.println("");
+        System.out.println("is greedy and will eat you on sight.");
+        System.out.println("");
+        System.out.println("which cave will you go into? "+ "\u001B (1 or 2)");
+        //Spacing
+        System.out.println("");
+        System.out.println("");
+        int answer;
+        //Make input function
+
+        //Spacing
+        System.out.println("");
+        System.out.println("");
+
+        //Make if(answer == 1){}
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+
+
+
+        return 0;
+    }
+
+}
---
+++ GenSparkProjects/src/GenProject/Main.java
@@ -0,0 +1,10 @@
+package GenProject;
+
+public class Main {
+    public static int main(String[] args) {
+        Dragons dargon = new Dragons();
+
+        return dargon.DragonsCave();
+
+    }
+}
